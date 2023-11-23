3f726e094c3202d402f261dc228d64eb70f98b02
Christopher Lecca <79271081+ChrisByBits@users.noreply.github.com>
2023-11-23 16:39:09 -0500

    3 queries inserted.js

--- universal-friends/non-relational-queries/u202211399.js
+++ universal-friends/non-relational-queries/u202211399.js
@@ -1,1 +1,29 @@
+// Calcula la cantidad de usuarios que comparten el mismo interés
+db.usuarios.aggregate([
+    {
+        $unwind: "$intereses"
+    },
+    {
+        $group: {
+            _id: { $toLower: "$intereses" },
+            cantidad_usuarios: { $count: {} }
+        }
+    },
+    {
+        $sort: { cantidad_usuarios: -1 }
+    }
+]);
 
+// Calcula la diferencia en días entre la fecha actual y la fecha de registro de cada usuario
+db.usuarios.aggregate([{
+    $project: {
+        _id: 1,
+        nombre: 1,
+        dias_desde_registro: {
+            $divide: [{
+                $subtract: [new Date(), "$fecha_registro"] },
+                1000 * 60 * 60 * 24
+            ]
+        }
+    }
+}]);
