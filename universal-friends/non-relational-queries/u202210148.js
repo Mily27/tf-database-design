04ef269add489cc2bc7e2c039fec348aa1707cbb
Mathias Kunimoto <102176303+kuni2005@users.noreply.github.com>
2023-11-23 19:32:57 -0500

    Update u202210148.js

--- universal-friends/non-relational-queries/u202210148.js
+++ universal-friends/non-relational-queries/u202210148.js
@@ -16,6 +16,26 @@ db.usuarios.updateOne(
     { $set: { contrasenia: 'TitoKuni' } }
 );
 
-/*Obtener Apellidos Paternos del docente */
-db.docentes.find({}, { "_id": 0, "apellido_paterno": 1 });
+/*Mostrar Apellido Paterno del Docente */
+db.docentes.aggregate([
+    {
+      $project: {
+        _id: 0,
+        apellido_paterno: 1
+      }
+    }
+]);
+
+
+/*Mostrar a todos los docentes */
+db.docentes.aggregate([
+    {
+      $project: {
+        _id: 1,
+        nombre: 1,
+        apellido_paterno: 1,
+        apellido_materno: 1
+      }
+    }
+  ]);
 
