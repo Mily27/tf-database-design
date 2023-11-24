9aeba9d5e1ff5768f596137b6ec4a864601ba1b5
Mathias Kunimoto <102176303+kuni2005@users.noreply.github.com>
2023-11-23 19:17:39 -0500

    Update u202213375.js

--- universal-friends/non-relational-queries/u202213375.js
+++ universal-friends/non-relational-queries/u202213375.js
@@ -1,1 +1,15 @@
+/*Insertar un docente nuevo */
+db.docentes.insertOne({
+    nombre: 'Alan',
+    apellido_paterno: 'Tito',
+    apellido_materno: 'Gutierrez'
+});
 
+/*Buscar Alumnos por carreras y dar con la info */
+db.usuarios.find({
+    $or: [
+        { carrera: 'Ingeniería Informática' },
+        { carrera: 'Medicina' },
+        { carrera: 'Administración de Empresas' }
+    ]
+});
