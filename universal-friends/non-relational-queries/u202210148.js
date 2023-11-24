85571f012cf22100a4e621a228ec8fcade0b346c
Mathias Kunimoto <102176303+kuni2005@users.noreply.github.com>
2023-11-23 19:14:51 -0500

    Update u202210148.js

--- universal-friends/non-relational-queries/u202210148.js
+++ universal-friends/non-relational-queries/u202210148.js
@@ -1,1 +1,21 @@
+/*Insertar un usuario nuevo */
+  db.usuarios.insertOne({
+    nombre: 'Mathias',
+    apellido_paterno: 'Kunimoto',
+    apellido_materno: 'Watanabe',
+    correo_electronico: 'math.kuni123@gmail.com',
+    contrasenia: '123456789',
+    carrera: 'Ingenieria de Futbol',
+    fecha_registro: new Date(),
+    intereses: ['Futbol', 'CR7']
+});
+
+/*Actualizar la contraseña de un usuario */
+db.usuarios.updateOne(
+    { nombre: 'Mathias' },
+    { $set: { contrasenia: 'TitoKuni' } }
+);
+
+/*Obtener Apellidos Paternos del docente */
+db.docentes.find({}, { "_id": 0, "apellido_paterno": 1 });
 
